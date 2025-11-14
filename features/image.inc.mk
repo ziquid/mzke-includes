80f342c25b084c10a8f5f2a92a88c4de7d16a14c
Joseph Cheek <joseph@ziquid.com>
2025-11-14 13:57:03 -0700

    allow for custom Dockerfile in IMAGE feature don't build all platforms if not on a PROD branch

--- features/image.inc.mk
+++ features/image.inc.mk
@@ -1,12 +1,16 @@
 # feature/image.inc.mk
+FEATURE_IMAGE := Y
+
+DOCKERFILE ?= Dockerfile
 
 .PHONY: bpa build
-bpa build: Dockerfile ddr ## Build and Push All custom Docker image(s) built by this app
+bpa build: $(DOCKERFILE) ddr ## Build and Push All custom Docker image(s) built by this app
 ifeq ($(IS_PROD),Y)
 	$(eval IMAGE_BUILD_PUSH := --push)
+	$(eval IMAGE_BUILD_PLATFORMS := --platform linux/amd64,linux/arm64)
 else
 	$(eval IMAGE_BUILD_PUSH := --load)
 	$(info Cowardly refusing to push a non-prod branch to container registry)
 endif
 	docker buildx use mybuilder
-	docker buildx build --platform linux/amd64,linux/arm64 $(IMAGE_BUILD_PUSH) -t $(IMAGE) .
+	docker buildx build -f $(DOCKERFILE) $(IMAGE_BUILD_PLATFORMS) $(IMAGE_BUILD_PUSH) -t $(IMAGE) .
